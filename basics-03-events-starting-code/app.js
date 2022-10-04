535a80a1120dd360a9d79f6f61a7e482e5675be2
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-10-04 14:04:47 +0200

    Add files via upload

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,24 +3,69 @@ const app = Vue.createApp({
     return {
       counter: 10,
       name:'',
-      confirmedName: ''
+      confirmedName: '',
+      //fullname:'',
+      lastName:''
     };
   },
+  watch:{
+    counter(value){
+      if(value>50){
+        const that = this;
+        setTimeout(function (){
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+    /* name(value){
+      if(value === ''){
+        this.fullname = '';  
+      } else {
+        this.fullname = value + ' ' + this.lastName;
+      }
+    },
+    lastName(value){
+      if(value === ''){
+        this.fullname = '';  
+      } else {
+        this.fullname = this.name + ' ' + value;
+      }
+    } */
+  },
+  computed: {
+    fullname(){
+      console.log('Again...');
+      if(this.name === '' || this.lastName === ''){
+        return '';
+      }
+      return this.name + ' ' + this.lastName; 
+    }
+  },
   methods: {
+    outputFullName(){
+      /* console.log('Again...');
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'Mansilla'; */
+    },
     confirmInput(){
       this.confirmedName = this.name;
     },
     submitForm(){
       alert('Submitted!');
     },
-    setName(event, lastName){
-      this.name = event.target.value + ' ' + lastName; 
+    setName(event){
+      this.name = event.target.value; 
     },
     add(num){
       this.counter = this.counter + num;
     },
     reduce(num){
       this.counter = this.counter - num;
+    },
+    resetInput(){
+      this.name = '';
     }
   }
 });
