bf7c0cc3439d090008e2606bb206499cac72df50
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-10-04 11:06:50 +0200

    Add files via upload

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,28 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
-      name:'',
-      confirmedName: ''
+      counter: 0,
     };
   },
-  methods: {
-    confirmInput(){
-      this.confirmedName = this.name;
-    },
-    submitForm(){
-      alert('Submitted!');
-    },
-    setName(event, lastName){
-      this.name = event.target.value + ' ' + lastName; 
-    },
-    add(num){
-      this.counter = this.counter + num;
-    },
-    reduce(num){
-      this.counter = this.counter - num;
-    }
-  }
 });
 
 app.mount('#events');
